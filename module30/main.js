eea959d8807b50990f93c6881e8c772f55d8259f
shpatberisha <shpatberisha2510@gmail.com>
2024-10-22 16:20:58 +0200

    Add files via upload

---
+++ module30/main.js
@@ -0,0 +1,12 @@
+var city ="prishtina"
+var age = 15
+
+if(city == "prishtina" &&  age>=18){
+    console.log("you are a citizen of Prishtina")
+}
+ else{
+    console.log("you are not a citizen of Prishtina")
+ }
+
+
+ var cars = ["Bmw" "Audi" ]
