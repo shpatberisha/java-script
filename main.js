343ed971efbfcbcfe5615d3c0764acfb70ca1431
shpatberisha <shpatberisha2510@gmail.com>
2024-11-05 16:14:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+var sum = 0;
+
+for(i=0; i<15; i++){
+    sum += list[i]
+}
+
+
+console.log(shuma)
+
+var sum2=0;
+for(x of list){
+    sum2+=x;
+}
+
+console.log(sum2)
