a143d4272dab8ddaa8d468b7e0a301e8507865f7
shpatberisha <shpatberisha2510@gmail.com>
2024-11-19 16:18:29 +0100

    lesson 35

---
+++ module31/Slide Show/main.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+
+
+function showSlides(n){
+    var slides = document.getElementsByClassName("slide");
+
+    for(i=0; i<slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    if(n>slides.length){
+        slideIndex = 1;
+    }
+    if(n<1){
+        slideIndex = slides.length;
+    }
+    slides[slideIndex-1].style.display = "block"
+}
+showSlides(1);
+
+function changeSlide(n){
+    slideIndex+=n;
+    showSlides(slideIndex);
+}
+
+setInterval(changeSlide, 3000, 1);
