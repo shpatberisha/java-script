fef7b912630f3ccf7721a3ed95d02883dc292aeb
shpatberisha <shpatberisha2510@gmail.com>
2024-11-28 18:08:39 +0100

    hamburger menu

---
+++ module31/Hamburger menu/main.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    var menu = document.getElementById("links");
+    if(menu.style.display == 'none'){
+        menu.style.display = "block";
+    }
+    else{
+        menu.style.display = "none";
+    }
+}
