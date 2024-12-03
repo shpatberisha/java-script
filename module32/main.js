ec070800e97aa15d13e4d2236f8dc1ef2af9645a
shpatberisha <shpatberisha2510@gmail.com>
2024-12-03 16:22:42 +0100

    shpati

---
+++ module32/main.js
@@ -0,0 +1,20 @@
+$("li").click(function(){
+    alert("you clicked me");
+});
+
+$("#btn").click(function(){
+    $("p").text("The paragraph content has been change");
+})
+
+$("#div1").click(function(){
+    $(this).addClass("class")
+   
+}).find("span").attr("data-example", "value");
+
+
+    
+        
+    
+
+    
+    
